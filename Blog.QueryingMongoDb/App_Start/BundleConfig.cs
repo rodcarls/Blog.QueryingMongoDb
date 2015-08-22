2a1ce4e3ea9ce6288b5bbc1fd1499cd5bd407432
Samuele Resca <samuele.resca@gmail.com>
2015-08-22 23:20:31 +0200

    Refactorying

---
+++ Blog.QueryingMongoDb/App_Start/BundleConfig.cs
@@ -0,0 +1,43 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace Blog.QueryingMongoDb
+{
+    public class BundleConfig
+    {
+        // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
+                        "~/Scripts/jquery-ui-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.unobtrusive*",
+                        "~/Scripts/jquery.validate*"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new StyleBundle("~/Description/css").Include("~/Description/site.css"));
+
+            bundles.Add(new StyleBundle("~/Conte/themes/base/css").Include(
+                        "~/Description/themes/base/jquery.ui.core.css",
+                        "~/Description/themes/base/jquery.ui.resizable.css",
+                        "~/Description/themes/base/jquery.ui.selectable.css",
+                        "~/Description/themes/base/jquery.ui.accordion.css",
+                        "~/Description/themes/base/jquery.ui.autocomplete.css",
+                        "~/Description/themes/base/jquery.ui.button.css",
+                        "~/Description/themes/base/jquery.ui.dialog.css",
+                        "~/Description/themes/base/jquery.ui.slider.css",
+                        "~/Description/themes/base/jquery.ui.tabs.css",
+                        "~/Description/themes/base/jquery.ui.datepicker.css",
+                        "~/Description/themes/base/jquery.ui.progressbar.css",
+                        "~/Description/themes/base/jquery.ui.theme.css"));
+        }
+    }
+}
---
+++ Blog.QueryingMongoDb/Models/Repository/ContactCollection.cs
@@ -0,0 +1,50 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Blog.QueryingMongoDb.Models.Repository
+{
+    public class ContactCollection
+    {
+
+        private MongoDbRepo _repo = new MongoDbRepo("mongodb://127.0.0.1:27017", "QueryMongoDb");
+
+        private string _collectionName = "ContactCollection";
+
+        public IMongoCollection<ContactModel> Collection;
+ 
+
+        public ContactCollection()
+        {
+            this.Collection = _repo.Db.GetCollection<ContactModel>(this._collectionName);
+        }
+
+
+        public void InsertContact(ContactModel contact)
+        {
+            this.Collection.InsertOneAsync(contact);
+        }
+
+        public List<ContactModel> SelectAll()
+        {
+            var query = this.Collection.Find(new BsonDocument()).ToListAsync();
+            return  query.Result;
+        }
+
+        public ContactModel Get(string id)
+        {
+            return this.Collection.Find(new BsonDocument { { "_id", new ObjectId(id) } }).FirstAsync().Result;
+        }
+
+        public void UpdateContact(string id, ContactModel contact)
+        {
+            contact.Id = new ObjectId(id);
+
+            var filter = Builders<ContactModel>.Filter.Eq(s => s.Id, contact.Id);
+            this.Collection.ReplaceOneAsync(filter, contact);
+        }
+    }
+}
